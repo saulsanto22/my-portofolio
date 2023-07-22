4b9d37c1e77eb407d99ba88bfce6d8c60be8dac3
GervazioMangu <vaziownm@gmail.com>
2023-07-22 20:32:32 +0700

    menambahkan bg pada css

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 //event saat link di klik
-$('.page-scroll').on('click', function(e){
+$(".page-scroll").on("click", function (e) {
+  //ambil isi hreff
+  var tujuan = $(this).attr("href");
+  //tangkap elemen ybs
+  var elemenTujuan = $(tujuan);
 
-    //ambil isi hreff 
-    var tujuan = $(this).attr('href');
-    //tangkap elemen ybs
-    var elemenTujuan = $(tujuan);
+  //pindahkan scroll
+  $("body").animate(
+    {
+      scrollTop: elemenTujuan.offset().top - 100,
+    },
+    1000,
+    easeInOutExpo
+  );
 
-    //pindahkan scroll
-    $('body').animate({
-        scrollTop: elemenTujuan.offset().top - 100         
-    }, 1000, easeInOutExpo);
-
-    e.preventDefault();
-
+  e.preventDefault();
 });
